2c2f6461719bb288f28cc2a051f76b8482168eae
rzsamrio <samuelekop144@gmail.com>
2023-02-16 16:40:42 +0100

    C - Hello world | advanced

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+/*
+ * main - pronts text and stderr material
+ * void: no cli required
+ * Return: 1
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
