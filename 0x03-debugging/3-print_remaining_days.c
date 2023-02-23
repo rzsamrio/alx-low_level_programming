8ea5e4b0cd6f149b805d967e59207190ef03fd4e
rzsamrio <samuelekop144@gmail.com>
2023-02-23 21:02:39 +0100

    C - debugging | c3

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -12,11 +12,9 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-	int y = year;
-
-	if (((y % 100 == 0) && (y % 400 == 0)) || ((y % 100 != 0) && (y % 4 == 0)))
+	if ((year % 4 == 0 || year % 400 == 0))
 	{
-		if (month >= 2 && day >= 60)
+		if (month > 2 && day >= 60)
 		{
 			day++;
 		}
@@ -37,4 +35,3 @@ void print_remaining_days(int month, int day, int year)
 		}
 	}
 }
-
