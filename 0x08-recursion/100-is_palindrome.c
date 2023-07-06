e085da40298f4c46409a0ac9a0634fcfa74f2783
rzsamrio <samuelekop144@gmail.com>
2023-07-06 15:24:12 +0100

    C recursion, advanced | add betty

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,15 +1,19 @@
+/** 
+ * Pseudocode
+ * save reverse string in an array with recursion
+ * write function that compares 2 strings and returns 1 if equal
+ * compare 2 strings
+ *
+ * Learnt more about the memory allocation on the stack (aka static malloc)
+ * while working this
+*/
+
 #include "main.h"
 
 /**
- * 
-*/
-
-/** Pseudocode
- *  save reverse string in an array with recursion
- *  write function that compares 2 strings and returns 1 if equal
- *  compare 2 strings 
- * 
- * Learnt more about the memory allocation on the stack (aka static malloc) while working this
+ * save - saves a copy of a string using recursion
+ * @s: address of string to clone
+ * @copy: copied string
 */
 
 void save(char *s, char *fwd)
@@ -22,6 +26,14 @@ void save(char *s, char *fwd)
 	save(s + 1, fwd + 1);
 	*fwd = *s;
 }
+
+/**
+ * rsave - reverses and stores a string using recursion
+ * @head: saved address of the first byte of the original string
+ * @s: original string address that becomes mobile
+ * @copy: address of the stored reverse string
+*/
+
 void rsave(char *head, char *s, char *rev)
 {
 	if (s < head)
@@ -33,6 +45,12 @@ void rsave(char *head, char *s, char *rev)
 	*rev = *s;
 }
 
+/**
+ * _strlen - calculates the length of a string via recursion
+ * @s: the address of the string
+ * Return: string length
+*/
+
 int _strlen(char *s)
 {
 	if (!*s)
@@ -42,6 +60,13 @@ int _strlen(char *s)
 	else
 		return (1 + _strlen(++s));
 }
+
+/**
+ * _strcmp - checks if 2 strings are identical
+ * @s1: address of first string
+ * @s2: address of the second string
+ * Return: 0 if false, 1 if true
+*/
 
 int _strcmp(char *s1, char *s2)
 {
@@ -53,6 +78,11 @@ int _strcmp(char *s1, char *s2)
 		return (1 * _strcmp(++s1, ++s2));
 }
 
+/**
+ * is_palindrome - the main function, checks if a string is a palindrome
+ * @s: address of string to screen
+ * Return: 1 if true and 0 if not
+*/
 int is_palindrome(char *s)
 {
 	char rev[500];
