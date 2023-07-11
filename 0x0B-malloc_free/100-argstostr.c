6466a38e0d4d90b277028a601b9989a34ccf691c
rzsamrio <samuelekop144@gmail.com>
2023-07-11 14:06:50 +0100

    C malloc, advanced

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * *argstostr - catenates all arguments in a string with \n
+ * @ac: number of command line arguments
+ * @av: array containing arguments
+ * Return: address of the string
+*/
+
+char *argstostr(int ac, char **av)
+{
+	int i, j, c, length;
+	char *argcat;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	length = 0;
+	/* store total length of chars and individual strings*/
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+			length++;
+	}
+	argcat = malloc((sizeof(char) * length) + (sizeof(char) * ac));
+	if (argcat == NULL)
+		return (NULL);
+	for (i = 0, c = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			argcat[c] = av[i][j];
+			c++;
+			if (av[i][j + 1] == '\0')
+			{
+				argcat[c] = '\n';
+				c++;
+			}
+		}
+	}
+	argcat[length + ac - 1] = '\0';
+	return (argcat);
+}
