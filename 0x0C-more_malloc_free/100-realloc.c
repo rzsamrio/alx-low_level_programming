050c6457a6f924ca42338051d113c62aa285d409
rzsamrio <samuelekop144@gmail.com>
2023-07-14 15:04:09 +0100

    code fix

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -26,7 +26,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (NULL);
 	}
 	mptr = malloc(new_size);
-	for (i = 0; i < new_size; i++)
+	for (i = 0; i < new_size && i < old_size; i++)
 		((char *) mptr)[i] = ((char *)ptr)[i];
 	free(ptr);
 	return (mptr);
