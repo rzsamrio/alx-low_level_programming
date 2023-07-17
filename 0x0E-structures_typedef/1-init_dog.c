0a5d7ca487b025e3720e7e89a1b890fa6dad1422
rzsamrio <samuelekop144@gmail.com>
2023-07-17 12:29:05 +0100

    C structures, Task 1 | memory testing | Wvoid fix

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -10,9 +10,10 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d == NULL || name == NULL || owner == NULL)
-		return (NULL);
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+	if (d != NULL && name != NULL && owner != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
