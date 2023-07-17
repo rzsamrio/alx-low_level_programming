d49a68734c347f0193f62684e1dec463880e3380
rzsamrio <samuelekop144@gmail.com>
2023-07-17 13:16:23 +0100

    quick test

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -46,38 +46,26 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog;
 
+	if (name == NULL || owner == NULL)
+		return (NULL);
 	dog = malloc(sizeof(dog_t)); /* save memory for dog */
 	if (dog == NULL)
 		return (NULL);
-
-	if (name != NULL) /* Passing null to _strcpy results in undefined behaviour */
+	dog->name = malloc(_strlen(name) + 1); /*save memory for */
+	if (dog->name == NULL)
 	{
-		/* allocate memory only if name is not null */
-		dog->name = malloc(_strlen(name) + 1);
-		if (dog->name == NULL)
-		{
-			free(dog);
-			return (NULL);
-		}
+		free(dog);
+		return (NULL);
 	}
-	else /* ...so it is best to simply make our dest = null on null source */
-		dog->name = NULL;
-
-	if (owner != NULL) /* same here */
+	dog->owner = malloc(_strlen(name) + 1);
+	if (dog->owner == NULL)
 	{
-		dog->owner = malloc(_strlen(name) + 1);
-		if (dog->owner == NULL)
-		{
-			free(dog->name);
-			free(dog);
-			return (NULL);
-		}
+		free(dog->name);
+		free(dog);
+		return (NULL);
 	}
-	else
-		dog->owner = NULL;
-
-	dog->name = _strcpy(dog->name, name);
+	_strcpy(dog->name, name);
 	dog->age = age;
-	dog->owner = _strcpy(dog->owner, owner);
+	_strcpy(dog->owner, owner);
 	return (dog);
 }
