9160a8f7d8f85e9e0015b2a22891a140875d981d
rzsamrio <samuelekop144@gmail.com>
2023-07-17 11:57:49 +0100

    extra fixes (syntax + code)

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -2,14 +2,11 @@
 
 /**
  * main - displays the name of the file
- * @argc: number of argumentss
- * @argv: array containing arguments
  * Return: 0 on completion
 */
 
-int main(int argc, char *argv[])
+int main(void)
 {
-	(void) argc;
-	printf("%s\n", argv[0]);
+	printf(__FILE__);
 	return (0);
 }
--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,6 @@
 #ifndef FMACRO_H
 #define FMACRO_H
 
-#define ABS(x) ((x < 0) ? (x * -1) : x)
+#define ABS(x) (((x) < (0)) ? ((x) * (-1)) : (x))
 
 #endif
