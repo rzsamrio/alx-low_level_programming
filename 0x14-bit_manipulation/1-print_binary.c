e382b5640c08d7d9da469be7754fea740a0a3fe7
rzsamrio <samuelekop144@gmail.com>
2023-08-03 20:37:41 +0100

    fix 1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -10,13 +10,27 @@
 void print_binary(unsigned long int n)
 {
 	unsigned long int tmp;
+	int czero, c;
 
 	tmp = 0;
-	while (n)
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+	for (czero = 0, c = 0; n > 0;)
 	{
 		tmp <<= 1;
 		if ((n & 1U) == 1)
+		{
+			c = 1;
 			tmp++;
+		}
+		else
+		{
+			if (c == 0)
+				czero++;
+		}
 		n >>= 1;
 	}
 	while (tmp)
@@ -27,5 +41,7 @@ void print_binary(unsigned long int n)
 			_putchar('0');
 		tmp >>= 1;
 	}
+	for (c = 0; c < czero; c++)
+		_putchar('0');
 }
 
