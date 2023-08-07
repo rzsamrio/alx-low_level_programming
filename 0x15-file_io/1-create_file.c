81883bdf034b8ff0ec22eff668dfbf4993d302e7
rzsamrio <samuelekop144@gmail.com>
2023-08-07 20:56:31 +0100

    write handling

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,27 +10,23 @@
 int create_file(const char *filename, char *text_content)
 {
 
-	int i, wc, fd;
+	int fd;
 
 	if (filename == NULL)
 		return (-1);
+	if (text_content == NULL)
+		text_content = "";
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
-	for (i = 0, wc = 0; *text_content != '\0'; i++)
+	while(*text_content != '\0')
 	{
 		if ((write(fd, text_content, 1)) == -1)
 		{
 			close(fd);
 			return (1);
 		}
-		wc++;
 		text_content++;
-	}
-	if (wc != i)
-	{
-		close(fd);
-		return (-1);
 	}
 	close(fd);
 	return (1);
