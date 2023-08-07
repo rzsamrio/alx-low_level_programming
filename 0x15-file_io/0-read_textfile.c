a390f06da0b1bd7681571587ebcbc3c5a4a00d6c
rzsamrio <samuelekop144@gmail.com>
2023-08-07 20:15:00 +0100

    Null case

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -14,7 +14,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t rc, wc;
 	char buffer[MAX_LEN];
 
-	if (filename == NULL)
+	if (filename == NULL || buffer == NULL)
 		return (0);
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
@@ -22,7 +22,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	rc = read(fd, buffer, letters);
 	if (rc == -1)
 	{
-		close (fd);
+		close(fd);
 		return (0);
 	}
 	wc = write(STDOUT_FILENO, buffer, rc);
