0bf7f455cbed003c93ccd805baa439bf5f7435fc
rzsamrio <samuelekop144@gmail.com>
2023-08-07 21:04:04 +0100

    fix betty

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,7 +19,7 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
-	while(*text_content != '\0')
+	while (*text_content != '\0')
 	{
 		if ((write(fd, text_content, 1)) == -1)
 		{
