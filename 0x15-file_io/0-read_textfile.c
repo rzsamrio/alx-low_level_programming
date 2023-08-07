e3fd8a7697f6ed1d5ba18cc83ecc4e950bf5024e
rzsamrio <samuelekop144@gmail.com>
2023-08-07 20:22:30 +0100

    var case

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -12,13 +12,19 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
 	ssize_t rc, wc;
-	char buffer[MAX_LEN];
+	char *buffer;
 
-	if (filename == NULL || buffer == NULL)
+	if (filename == NULL)
 		return (0);
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
+	buffer = malloc(letters);
+	if (buffer == NULL)
+	{
+		close(fd);
+		return (0);
+	}
 	rc = read(fd, buffer, letters);
 	if (rc == -1)
 	{
@@ -31,6 +37,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		close(fd);
 		return (0);
 	}
+	free(buffer);
 	close(fd);
 	return (wc);
 }
