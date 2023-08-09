848d70e6a217dce8a61ea6298695e4ba375ebb23
rzsamrio <samuelekop144@gmail.com>
2023-08-09 15:19:47 +0100

    fix task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -24,27 +24,30 @@ int main(int argc, char *argv[])
 	buffer = malloc(buf_size);
 	if (buffer == NULL)
 		return (1);
-	for (rc = 0; (len = read(fdf, buffer, buf_size)) > 0; rc += len)
+	for (rc = 0; (len = read(fdf, &buffer[rc], buf_size)) > 0; rc += len)
 	{
+		if (len == -1)
+		{
+			close_buffer(fdf, buffer);
+			err_msg(98, argv[1]);
+		}
 		if (len == buf_size)
 			buf_size *= 2;
 		buffer = realloc(buffer, buf_size);
 		if (buffer == NULL)
 			return (1);
 	}
-	if (len == -1)
-	{
-		free(buffer);
-		close(fdf);
-		err_msg(98, argv[1]);
-	}
 	fdt = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fdt == -1)
+	{
+		close_buffer(fdf, buffer);
 		err_msg(99, argv[2]);
+	}
 	wc = write(fdt, buffer, rc);
+	close_buffer(fdf, NULL);
+	close_buffer(fdt, buffer);
 	if (wc != rc)
 		err_msg(99, argv[2]);
-	free(buffer);
 	return (0);
 }
 
@@ -57,19 +60,33 @@ void err_msg(int err_flag, char *s)
 {
 	switch (err_flag)
 	{
-		case (-1):
-			free(s);
-			dprintf(2, "Error: Can't write to %s\n", s);
-			exit(98);
 		case (97):
-			dprintf(2, "Usage: cp file_from file_to\n");
+			dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 			exit(97);
 		case (98):
-			dprintf(2, "Error: Can't read from file %s\n", s);
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", s);
 			exit(98);
 		case (99):
-			dprintf(2, "Error: Can't write to %s\n", s);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", s);
 			exit(99);
 	}
 }
 
+/**
+ * close_buffer - closes an fd and frees a buffer
+ * @fd : file descriptor
+ * @buffer: dynamic string
+ */
+
+void close_buffer(int fd, char *buffer)
+{
+	int status;
+
+	free(buffer);
+	status = close(fd);
+	if (status == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", fd);
+		exit(100);
+	}
+}
