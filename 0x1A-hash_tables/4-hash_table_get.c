6dd9d4b0f15fa5b318735f1bb06f2294e851337b
rzsamrio <samuelekop144@gmail.com>
2023-10-07 02:41:42 +0100

    include guards and safe check

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -11,10 +11,13 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int index;
 	hash_node_t *ptr;
 
+	if (key == NULL || strcmp(key, "") == 0)
+		return (NULL);
+	if (ht == NULL)
+		return (NULL);
+
 	index = key_index((const unsigned char *) key, ht->size);
 	ptr = ht->array[index];
-	if (ptr->next == NULL)
-		return (ptr->value);
 	while (ptr != NULL)
 	{
 		if (strcmp(ptr->key, key) == 0)
