e30dc05205db07c58c46741159b24e3d96bedc43
rzsamrio <samuelekop144@gmail.com>
2023-10-07 03:16:54 +0100

    add 5

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,32 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - Prints a hash table
+ * @ht: Table to print
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+	int set;
+	unsigned long int i;
+	hash_node_t *ptr;
+
+	if (ht == NULL)
+		return;
+
+	set = 0;
+	putchar('{');
+	for (i = 0; i < ht->size; i++)
+	{
+		ptr = ht->array[i];
+		while (ptr != NULL)
+		{
+			if (set != 0)
+				printf(", ");
+			printf("'%s': '%s'", ptr->key, ptr->value);
+			set = 1;
+			ptr = ptr->next;
+		}
+	}
+	printf("}\n");
+}
