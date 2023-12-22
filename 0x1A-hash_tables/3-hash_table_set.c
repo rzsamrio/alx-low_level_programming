375543044efbdc7bd90f0e195cdc51b0a9826552
rzsamrio <samuelekop144@gmail.com>
2023-12-22 16:21:25 +0100

    Instruction: _Value can be an empty string_

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -16,7 +16,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 	if (key == NULL || strcmp(key, "") == 0)
 		return (0);
-	if (value == NULL || strcmp(value, "") == 0)
+	if (value == NULL)
 		return (0);
 	index = key_index((const unsigned char *) key, ht->size);
 
