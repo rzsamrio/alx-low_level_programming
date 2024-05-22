433c7c4e9e27f0425eb12f304d9010d1ff9ec9d1
rzsamrio <samuelekop144@gmail.com>
2024-05-22 04:20:34 +0100

    [0x1E] add task 0

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * linear_search - Uses linear search algorithm to find an array element
+ * @array: Pointer to first element of the array
+ * @size: length of array
+ * @value: value to search for
+ * Return: Index of first occurrence of value
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+
+	if (array == NULL)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+	}
+
+	return (-1);
+}
